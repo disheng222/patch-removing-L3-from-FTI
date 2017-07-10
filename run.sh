06df2400c42899863bdfea1033ed3aad295c1c94
Sheng Di <sdi1@anl.gov>
2017-07-10 11:54:03 -0500

    first version

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+ftiDir=/home/sdi/Development/FTI/fti
+
+curDir=`pwd`
+
+cd $ftiDir
+
+echo patch -p0 < "$curDir/CMakeLists.txt.patch"
+patch -p0 < "$curDir/CMakeLists.txt.patch"
+
+cd src
+
+for file in `cd "$curDir/src";ls *.patch`
+do
+	echo patch -p0 < "$curDir/src/$file"
+	patch -p0 < "$curDir/src/$file"
+done
